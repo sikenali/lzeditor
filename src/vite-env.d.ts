fd7bceed82f797780d7b1590ea66ecaf6f295c5f
sikenali <mrchiang1015@aliyun.com>
2026-09-16 15:49:26 +0800

    fix: simplify vite-env.d.ts type declarations for react-katex and echarts

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,22 +1,31 @@
 /// <reference types="vite/client" />
-\ndeclare module 'react-katex' {
+
+declare module 'react-katex' {
   import * as React from 'react'
-  
   interface KatexProps {
     expression: string
     displayMode?: boolean
     throwOnError?: boolean
-    errorColor?: string
-    color?: string
-    maxSize?: number
-    maxExpand?: number
-    strict?: boolean | string
-    trust?: boolean | ((url: string) => boolean)
-    globalGroup?: boolean
-    output?: 'html' | 'mathml' | 'both'
     [key: string]: any
   }
-  
   class Katex extends React.Component<KatexProps> {}
   export = Katex
+}
+
+declare module 'echarts' {
+  export * from 'echarts'
+  const echarts: any
+  export default echarts
+}
+
+declare module 'echarts-for-react' {
+  import * as React from 'react'
+  interface EChartsReactProps {
+    option?: any
+    style?: React.CSSProperties
+    className?: string
+    [key: string]: any
+  }
+  class EChartsReact extends React.Component<EChartsReactProps> {}
+  export default EChartsReact
 }
